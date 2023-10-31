420765d77f628b97d52309961ecbe691bd4f025b
prathmesh4912 <prathmeshpawaru@gmail.com>
2023-10-31 18:11:08 +0530

    MONGO DB URL added in config/env

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,12 @@
 // getting-started.js
-
+const dotenv = require("dotenv") ;
 const mongoose = require("mongoose") ;
 
 // const mon_url ='mongodb+srv://Prath_4912:Ramila%404912@cluster0.czlgxbm.mongodb.net/ob' ;
-const mon_url ='mongodb://127.0.0.1:27017/Organic_Bananas' ;
+// const mon_url ='mongodb://127.0.0.1:27017/Organic_Bananas' ;
+dotenv.config({path : "./config/config.env"}) ;
+
+const mon_url = process.env.MONGO_URL ;
 
 connectMongo().catch(err => console.log(err));
 
