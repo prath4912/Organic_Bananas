beb9f67e8133aa532f5ba59c3ee50772eb3d8854
prathmesh4912 <prathmeshpawaru@gmail.com>
2023-11-02 10:40:17 +0530

    no

--- Backend/db.js
+++ Backend/db.js
@@ -3,10 +3,10 @@ const dotenv = require("dotenv") ;
 const mongoose = require("mongoose") ;
 
 // const mon_url ='mongodb+srv://Prath_4912:Ramila%404912@cluster0.czlgxbm.mongodb.net/ob' ;
-// const mon_url ='mongodb://127.0.0.1:27017/Organic_Bananas' ;
+const mon_url ='mongodb://127.0.0.1:27017/Organic_Bananas' ;
 dotenv.config({path : "./config/config.env"}) ;
 
-const mon_url = process.env.MONGO_URL ;
+// const mon_url = process.env.MONGO_URL ;
 
 connectMongo().catch(err => console.log(err));
 
